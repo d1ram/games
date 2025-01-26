1f31d2c42aa6b84dbba191d6f62fadaa46706b5f
d1ram <d1ram.komirol@gmail.com>
2025-01-27 00:33:18 +0100

    second

--- high/main.cpp
+++ high/main.cpp
@@ -21,25 +21,16 @@ void make_border_game(vector<int>& side) {
     side.insert(side.begin(), s); 
     side.pop_back();    
 }
+void game(string bor, string main_hero_left, string main_hero_right, string pole, int& score) {
+    nodelay(stdscr, TRUE);
+    keypad(stdscr, TRUE);
 
-int main() {
     int rows = 20;
     int columns = 10;
     int start_row = 10;
-    
-    string bor = "I";
-    string main_hero = "X";
-    string pole = " ";
-
-    initscr();
-    cbreak();
-    noecho();
-    nodelay(stdscr, TRUE);
-    keypad(stdscr, TRUE); 
 
     int direction = 2;
     int col = 1;
-
     vector<int> side;
 
     vector<vector<string>> polje(rows, vector<string>(columns, "."));
@@ -50,6 +41,7 @@ int main() {
     side[start_row] = 2;
 
     int floor = 0;
+    string hero = main_hero_right;
 
     bool has_border_left;
     bool has_border_right;
@@ -57,21 +49,20 @@ int main() {
 
     int left_side = 0;
     int right_side = 0;
-     
+
+    bkgd(COLOR_PAIR(2));
     // direction(1) == left, direction(2) == right;
     while (true) {
         int ch;
 
-        napms(100);
+        napms(75);
 
         clear(); 
 
         ch = getch();
 
-        main_hero = "o";
         if (ch == 'w') {
             make_border_game(side);
-            main_hero = "O";
             floor++;
         }
 
@@ -86,7 +77,6 @@ int main() {
                 polje[i][j] = pole;
             }
         }
-
         for (int i = 0; i < rows; i++) { // borders
             if (side[i] == 0) {
                 polje[i][0] = bor;
@@ -125,20 +115,172 @@ int main() {
         if (direction == 1 && col <= 1 && !has_two_borders || direction == 2 && col >= columns - 2 && !has_two_borders ) break;
 
         if (direction == 1) {
+            hero = main_hero_left;
             col--;
         } else if (direction == 2) {
+            hero = main_hero_right;
             col++;
         }
 
-        polje[start_row][col] = main_hero; 
+        polje[start_row][col] = hero;
 
         // printw("col: %d, direction: %d\n", col, direction);
         printw("Score: %d\n", floor);
-        if (floor % 10 == 0) {
+        if (floor % 10 == 0 && floor > score) {
+            printw("(#####  %d. New record! #####)", floor);
+        } else if (floor % 10 == 0) {
             printw("(#####  %d  #####)", floor);
         }
-
         refresh(); 
     }
+    if (floor > score) {
+        score = floor;
+    }
+    attroff(COLOR_PAIR(4));
+    nodelay(stdscr, FALSE);
+    keypad(stdscr, FALSE);
+}
+void settings(string& main_hero_left ,string& main_hero_right, string& bor, string& pole) {
+    string main_text = "Change character skin";
+    string border_text = "Change border skin";
+    string pole_text = "Change pole skin";
+    string default_text = "Reset to default settings";
+    string exit_text = "Back to menu";
+    int picked = 0;
+
+    while(true){
+        int ch;
+
+        clear();
+
+        if (picked == 0) {
+            main_text.append(" <==");
+        } else if (picked == 1) {
+            border_text.append(" <==");
+        } else if (picked == 2) {
+            pole_text.append(" <==");
+        } else if (picked == 3) {
+            default_text.append(" <==");
+        } else if (picked == 4) {
+            exit_text.append(" <==");
+        }
+
+        printw("%s\n", main_text.c_str());
+        printw("%s\n", border_text.c_str());
+        printw("%s\n", pole_text.c_str());
+        printw("%s\n", default_text.c_str());
+        printw("%s\n", exit_text.c_str());
+
+        ch = getch();
+
+        if (ch == 'w' && picked > 0) {
+            picked--;
+        } else if (ch == 's' && picked < 4) {
+            picked++;
+        }
+
+        if (ch == 10 && picked == 0) {
+            printw("Press the char on keyboard \n(LEFT SIDE)\n");
+            main_hero_left = getch();
+            printw("Press the char on keyboard \n(RIGHT SIDE)\n");
+            main_hero_right = getch();
+        } else if (ch == 10 && picked == 1) {
+            printw("Press the char on keyboard");
+            bor = getch();
+        } else if (ch == 10 && picked == 2) {
+            printw("Press the char on keyboard");
+            pole = getch();
+        } else if (ch == 10 && picked == 3) {
+            attron(COLOR_PAIR(2));
+            printw("Reset completed!");
+            attroff(COLOR_PAIR(2));
+
+            int d = getch();
+
+            main_hero_left = "<";
+            main_hero_left = ">";
+            bor = "|";
+            pole = " ";
+        } else if (ch == 10 && picked == 4) {
+            break;
+        }
+
+        main_text = "Change character skin";
+        border_text = "Change border skin";
+        pole_text = "Change pole skin";
+        default_text = "Reset to default settings";
+        exit_text = "Back to menu";
+    }
+}
+void menu() {
+    string game_text = "Game";
+    string settings_text = "Settings";
+    string exit_text = "Exit";
+
+    string main_hero_left = "<";
+    string main_hero_right = ">";
+    string bor = "|";
+    string pole = " ";
+
+    int score;
+    
+    int picked = 0;
+
+    while (true) {
+        bkgd(COLOR_PAIR(3));
+        clear();
+
+        game_text = "Game";
+        settings_text = "Settings";
+        exit_text = "Exit";
+
+        if (picked == 0) {
+            game_text.append(" <==");
+        } else if (picked == 1) {
+            settings_text.append(" <==");
+        } else if (picked == 2) {
+            exit_text.append(" <==");
+        }
+
+        printw("%s\n", game_text.c_str());
+        printw("%s\n", settings_text.c_str());
+        printw("%s\n", exit_text.c_str());
+        printw("Record: %d\n", score);
+
+        int ch = getch();
+
+        if (ch == 'w' && picked > 0) {
+            picked--;
+        } else if (ch == 's' && picked < 2) {
+            picked++;
+        }
+
+        if (ch == 10 && picked == 0) {
+            game(bor, main_hero_left, main_hero_right, pole, score);
+        } else if (ch == 10 && picked == 1) {
+            settings(main_hero_left, main_hero_right, bor, pole);
+        } else if (ch == 10 && picked == 2) {
+            break;
+        }
+
+        refresh();
+    }
+
     endwin();
+}
+
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+
+    start_color();
+
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+
+    init_pair(2, COLOR_BLACK, COLOR_CYAN);
+
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+
+    menu();
 }
